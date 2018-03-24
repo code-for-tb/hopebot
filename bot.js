376fe81e54582e400f641d7779566ab346b1c874
Michael Bishop <miklb@miklb.com>
2018-03-24 01:22:19 -0400

    clean up

--- bot.js
+++ bot.js
@@ -1,37 +1,20 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+/**
+ * Copyright 2016 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-
-This is a sample Facebook bot built with Botkit.
-
-# RUN THE BOT:
-  Follow the instructions here to set up your Facebook app and page:
-    -> https://developers.facebook.com/docs/messenger-platform/implementation
-  Run your bot from the command line:
-    page_token=<MY PAGE TOKEN> verify_token=<MY_VERIFY_TOKEN> node bot.js
-
-
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-var env = require('node-env-file');
-env(__dirname + '/.env');
-
-
-if (!process.env.page_token) {
-    console.log('Error: Specify a Facebook page_token in environment.');
-    usage_tip();
-    process.exit(1);
-}
-
-if (!process.env.verify_token) {
-    console.log('Error: Specify a Facebook verify_token in environment.');
-    usage_tip();
-    process.exit(1);
-}
+require('dotenv').load();
 
 var middleware = require('botkit-middleware-watson')({
   username: process.env.CONVERSATION_USERNAME,
@@ -41,7 +24,26 @@ var middleware = require('botkit-middleware-watson')({
   version_date: '2017-05-26'
 });
 
-// Customize your Watson Middleware object's before and after callbacks.
+module.exports = function(app) {
+  if (process.env.USE_SLACK) {
+    var Slack = require('./bot-slack');
+    Slack.controller.middleware.receive.use(middleware.receive);
+    Slack.bot.startRTM();
+    console.log('Slack bot is live');
+  }
+  if (process.env.USE_FACEBOOK) {
+    var Facebook = require('./bot-facebook');
+    Facebook.controller.middleware.receive.use(middleware.receive);
+    Facebook.controller.createWebhookEndpoints(app, Facebook.bot);
+    console.log('Facebook bot is live');
+  }
+  if (process.env.USE_TWILIO) {
+    var Twilio = require('./bot-twilio');
+    Twilio.controller.middleware.receive.use(middleware.receive);
+    Twilio.controller.createWebhookEndpoints(app, Twilio.bot);
+    console.log('Twilio bot is live');
+  }
+  // Customize your Watson Middleware object's before and after callbacks.
   middleware.before = function(message, conversationPayload, callback) {
     callback(null, conversationPayload);
   }
@@ -50,85 +52,3 @@ var middleware = require('botkit-middleware-watson')({
     callback(null, conversationResponse);
   }
 };
-
-var Botkit = require('botkit');
-var debug = require('debug')('botkit:main');
-
-// Create the Botkit controller, which controls all instances of the bot.
-var controller = Botkit.facebookbot({
-    // debug: true,
-    verify_token: process.env.verify_token,
-    access_token: process.env.page_token,
-    studio_token: process.env.studio_token,
-    studio_command_uri: process.env.studio_command_uri,
-});
-
-// Set up an Express-powered webserver to expose oauth and webhook endpoints
-var webserver = require(__dirname + '/components/express_webserver.js')(controller);
-
-// Tell Facebook to start sending events to this application
-require(__dirname + '/components/subscribe_events.js')(controller);
-
-// Set up Facebook "thread settings" such as get started button, persistent menu
-require(__dirname + '/components/thread_settings.js')(controller);
-
-
-// Send an onboarding message when a user activates the bot
-require(__dirname + '/components/onboarding.js')(controller);
-
-// Load in some helpers that make running Botkit on Glitch.com better
-require(__dirname + '/components/plugin_glitch.js')(controller);
-
-// enable advanced botkit studio metrics
-require('botkit-studio-metrics')(controller);
-
-var normalizedPath = require("path").join(__dirname, "skills");
-require("fs").readdirSync(normalizedPath).forEach(function(file) {
-  require("./skills/" + file)(controller);
-});
-
-
-// This captures and evaluates any message sent to the bot as a DM
-// or sent to the bot in the form "@bot message" and passes it to
-// Botkit Studio to evaluate for trigger words and patterns.
-// If a trigger is matched, the conversation will automatically fire!
-// You can tie into the execution of the script using the functions
-// controller.studio.before, controller.studio.after and controller.studio.validate
-if (process.env.studio_token) {
-    controller.on('message_received,facebook_postback', function(bot, message) {
-        if (message.text) {
-            controller.studio.runTrigger(bot, message.text, message.user, message.channel, message).then(function(convo) {
-                if (!convo) {
-                    // no trigger was matched
-                    // If you want your bot to respond to every message,
-                    // define a 'fallback' script in Botkit Studio
-                    // and uncomment the line below.
-                    controller.studio.run(bot, 'fallback', message.user, message.channel, message);
-                } else {
-                    // set variables here that are needed for EVERY script
-                    // use controller.studio.before('script') to set variables specific to a script
-                    convo.setVar('current_time', new Date());
-                }
-            }).catch(function(err) {
-                if (err) {
-                    bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
-                    debug('Botkit Studio: ', err);
-                }
-            });
-        }
-    });
-} else {
-    console.log('~~~~~~~~~~');
-    console.log('NOTE: Botkit Studio functionality has not been enabled');
-    console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
-}
-
-function usage_tip() {
-    console.log('~~~~~~~~~~');
-    console.log('Botkit Studio Starter Kit');
-    console.log('Execute your bot application like this:');
-    console.log('page_token=<MY PAGE TOKEN> verify_token=<MY VERIFY TOKEN> studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js');
-    console.log('Get Facebook token here: https://developers.facebook.com/docs/messenger-platform/implementation')
-    console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
-    console.log('~~~~~~~~~~');
-}
